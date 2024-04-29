7e6b2f652429dd6a2c3e8fe9a1e3f1fe244960e7
JSedillos18@gmail.com <JSedillos18@gmail.com>
2024-04-28 22:01:57 -0700

    X-axis movement script add

---
+++ Hamster Hustle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+// Script based off roll-a-ball movement tutorial
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    public float speed = 10; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>(); 
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;   
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, 0.0f);
+        rb.AddForce(movement * speed); 
+    }
+
+}
