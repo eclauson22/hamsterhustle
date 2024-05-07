8b549048d2de09e038a433052315ec96d107f5ea
SawyerChristensen <schr5498@gmail.com>
2024-05-06 17:37:46 -0700

    improved obstacle spawn script
    now use three int values for three lanes

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -4,8 +4,8 @@ public class ObstacleGenerator : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     public Transform player;
-    public float spawnDistance = 5f;
-    public float spawnHeight = 0f;
+    public float spawnDistance = 3f;
+    public float spawnHeight = 1f;
 
     private static bool hasStarted = false;
 
@@ -22,10 +22,11 @@ public class ObstacleGenerator : MonoBehaviour
 
     void GenerateObstacle()
     {
-        Vector3 spawnPosition = player.position + player.forward; // * spawnDistance + Vector3.up * spawnHeight;
+        Vector3 spawnPosition = player.position + player.forward; // * spawnDistance + Vector3.up * spawnHeight; //the commented out code make them above the hamster
+        spawnPosition.y -= 1.2f;
         GameObject obstacleInstance = Instantiate(obstaclePrefab, spawnPosition, Quaternion.identity);
-        float randomOffset = Random.Range(-5f, 5f);
-        obstacleInstance.transform.Translate(Vector3.right * randomOffset);
+        int randomOffset = Random.Range(-1, 1); //choose between -1, 0, and 1, for three lanes instead of any float in between -1 and 1. used to also be between -5 and 5
+        obstacleInstance.transform.Translate(Vector3.right * randomOffset);// could multiply random_offset by 1.2 or something to increase the size of lanes, more left or more right. -1 becomes -1.3 left or 1 becomes 1.3 right
     }
 
     // Update is called once per frame
