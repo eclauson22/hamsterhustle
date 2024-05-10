bde34b6e36c58bcad84eea3fa2049a2ecef2c215
JSedillos18@gmail.com <JSedillos18@gmail.com>
2024-05-09 17:57:58 -0700

    NewMovement
    Changing positions movement instead of physics movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-//using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,12 +7,27 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private float movementX;
-    public float speed = 0; 
+    public float move_speed = 10.0f;
+    public float jumping_height = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody>(); 
+    }
+
+    private bool isGrounded;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wheel"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 
     void OnMove(InputValue movementValue)
@@ -22,10 +36,21 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;   
     }
 
+    void OnJump()
+    {
+        //Jumping movement
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumping_height, ForceMode.Impulse); 
+        }
+    }
+
     private void FixedUpdate() 
     {
-        Vector3 movement = new Vector3 (movementX, 0.0f, 0.0f);
-        rb.AddForce(movement * speed); 
+        Vector3 left_right_movement = new Vector3 (movementX, 0.0f, 0.0f);
+        //Apply the movement vector to the current position, which is
+        //multiplied by deltaTime and speed for a smooth MovePosition
+        rb.MovePosition(transform.position + left_right_movement * Time.deltaTime * move_speed);
     }
 
 }
