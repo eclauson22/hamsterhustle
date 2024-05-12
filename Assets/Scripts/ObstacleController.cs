3169e677344a9b53b32d525d1bddf037149b9f6c
elizagb <egerblack@gmail.com>
2024-05-11 19:25:20 -0700

    Multiple types of power ups and obstacles

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
-    public GameObject powerUpPrefab; 
+    public GameObject twigPrefab;
+    public GameObject rockPrefab;
+    public GameObject cheesePrefab;
+    public GameObject carrotPrefab;
+    public GameObject redbullPrefab;
+
     public Transform wheel;
     public float spawnDistance = 3f;
     public float spawnHeight = 1f;
-    public float obstacleProbability = 0.7f; // Probability of spawning an obstacle (vs. a power-up)
 
+    public float obstacleProbability = 0.7f; // Probability of spawning an obstacle (vs. a power-up)
+    public float redbullProbability = 0.2f;
 
     private static bool hasStarted = false;
 
@@ -30,7 +35,25 @@ public class ObstacleGenerator : MonoBehaviour
         Vector3 fixedSpawnPosition = new Vector3(50f, 14.5f, 24f);
 
         // Randomly determine if we should spawn an obstacle or a power-up
-        GameObject collectiblePrefab = Random.value < obstacleProbability ? obstaclePrefab : powerUpPrefab;
+        GameObject collectiblePrefab;
+        float randomValue = Random.value;
+        if (randomValue < obstacleProbability)
+        {
+            // Spawn an obstacle
+            collectiblePrefab = Random.value < 0.5f ? twigPrefab : rockPrefab;
+        }
+        else
+        {
+            // Spawn a power-up
+            if (randomValue < obstacleProbability + redbullProbability)
+            {
+                collectiblePrefab = redbullPrefab;
+            }
+            else
+            {
+                collectiblePrefab = Random.value < 0.5f ? cheesePrefab : carrotPrefab;
+            }
+        }
 
         // Instantiate the collectible at the fixed spawn position
         GameObject collectibleInstance = Instantiate(collectiblePrefab, fixedSpawnPosition, Quaternion.identity);
@@ -48,10 +71,9 @@ public class ObstacleGenerator : MonoBehaviour
         Vector3 collectiblePosition = fixedSpawnPosition + Vector3.right * laneOffset;
         collectibleInstance.transform.position = collectiblePosition;
 
-        string type = collectiblePrefab == obstaclePrefab ? "Obstacle" : "Power-up";
+        string type = collectiblePrefab == twigPrefab || collectiblePrefab == rockPrefab ? "Obstacle" : "Power-up";
         Debug.Log(type + " spawned in lane: " + laneOffset);
     }
-
 
 
 
