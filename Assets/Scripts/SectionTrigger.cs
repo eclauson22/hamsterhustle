da4dfb2925e620a319ffb53f742be5794807f6d8
eclauson22 <eclauson@uoregon.edu>
2024-05-12 22:15:40 -0700

    added speed increments

--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class SectionTrigger : MonoBehaviour
 {
     private GameManager gameManager; // Reference to the GameManager instance
-
+    //public WheelBehavior wheelBehavior;
     // public GameObject Environment;
-
     private void Start()
     {
         gameManager = GameManager.Instance; // Assign the GameManager instance to the reference
@@ -19,9 +18,10 @@ public class SectionTrigger : MonoBehaviour
         if (other.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Collision with obstacle!");
-            
+            //wheelBehavior.IncreaseSpeed(); 
             gameManager.HandleObstacleCollision();
             Destroy(other.gameObject); // Destroy the obstacle
         }
     }
+
 }
