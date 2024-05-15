f13052c88f51d00767cf4f8d8ca3c119f9c28719
elizagb <egerblack@gmail.com>
2024-05-15 11:49:09 -0700

    Shortened hamster movement bounds; cleaned up objects

--- Assets/Scripts/Bounds.cs
+++ Assets/Scripts/Bounds.cs
@@ -13,8 +13,8 @@ public class HamsterBounds : MonoBehaviour
     void Start()
     {
         // Initialize maxLeftPosition and maxRightPosition
-        maxLeftPosition = origin.x - 4f;
-        maxRightPosition = origin.x + 4f;
+        maxLeftPosition = origin.x - 3f;
+        maxRightPosition = origin.x + 3f;
     }
 
     // Update is called once per frame
