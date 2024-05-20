fb749dd62988882cc6179bdafd4db4ca5273cca2
JSedillos18@gmail.com <JSedillos18@gmail.com>
2024-05-19 17:53:52 -0700

    Invincibility Cheese
    Getting cheese makes hamster chnage to rainbow colors and and sound byte plays

--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -7,10 +7,12 @@ public class SectionTrigger : MonoBehaviour
     private GameManager gameManager; 
     private Renderer screenRenderer;
     private Color originalColor;
+    private RainbowColors rainbowColors;
 
     // Assigned in unity editor
     public AudioSource obstacleAudioSource; 
-    public AudioSource powerupAudioSource; 
+    public AudioSource powerupAudioSource;
+    public AudioSource cheeseAudioSource;  
 
 
     private void Start()
@@ -20,13 +22,22 @@ public class SectionTrigger : MonoBehaviour
         // For player red flash (Credit: Chat GPT)
         screenRenderer = GetComponent<Renderer>(); 
         originalColor = screenRenderer.material.color; // Store the original color
+        rainbowColors = GetComponent<RainbowColors>();
 
     }
 
-
+    // Is a Power up since only power ups use triggers
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("PowerUp"))
+        if (other.gameObject.CompareTag("Cheese"))
+        {
+            Debug.Log("Collision with Cheese power-up!");
+            gameManager.HandlePowerUpCollision();
+            cheeseAudioSource.Play();
+            rainbowColors.StartRainbowCycle();
+            Destroy(other.gameObject); // Destroy the obstacle
+        }
+        else
         {
             Debug.Log("Collision with power-up!");
             gameManager.HandlePowerUpCollision();
@@ -35,7 +46,7 @@ public class SectionTrigger : MonoBehaviour
         }
     }
 
-
+    // Not a power up since power ups do not use collisions
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
---
+++ Assets/hamster-3d-model/RainbowColors.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// "Unity - Cycling An Object Through Rainbow Colors", Youtube Tutorial Video
+public class RainbowColors : MonoBehaviour
+{
+    public Material mt;
+    private Color32[] colors;
+    private Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mt = transform.GetComponent<MeshRenderer>().material;
+        originalColor = mt.color;
+        colors = new Color32[7]
+        {
+            new Color32(255, 0, 0, 255), //red
+            new Color32(255, 165, 0, 255), //orange
+            new Color32(255, 255, 0, 255), //yellow
+            new Color32(0, 255, 0, 255), //green
+            new Color32(0, 0, 255, 255), //blue
+            new Color32(75, 0, 130, 255), //indigo
+            new Color32(238, 130, 238, 255), //violet
+        };
+    }
+
+    public IEnumerator Cycle()
+    {
+        int i = 0;
+        float timer = 0f;
+
+        while(timer < 14f)
+        {
+            for(float interpolant = 0f; interpolant < 1f; interpolant+= 0.01f)
+            {
+                mt.color = Color.Lerp(colors[i%7], colors[(i+1)%7], interpolant);
+                yield return null;
+                timer += Time.deltaTime; // increment timer by time since last frame
+            }
+            i++;
+        }
+
+        Debug.Log("Rainbow effect stoppped after 14 seconds");
+        mt.color = originalColor;
+    }
+
+    public void StartRainbowCycle()
+    {
+        StartCoroutine(Cycle());
+    }
+}
