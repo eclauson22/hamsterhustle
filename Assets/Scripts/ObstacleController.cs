23a2c1d106b9ce2d127a954408b8056a86e2c24c
elizagb <egerblack@gmail.com>
2024-05-20 09:37:54 -0700

    PowerUp locations & movement
    Redbulls bob up and down; cheese is higher up, so plater has to jump to get it; carrots are lower, near the wheel

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -31,15 +31,17 @@ public class ObstacleGenerator : MonoBehaviour
 
     void GenerateObstacle()
     {
-        // Fixed spawn position
+
+        // Default spawn position
         Vector3 fixedSpawnPosition = new Vector3(50f, 14.5f, 24f);
+
 
         // Randomly determine if we should spawn an obstacle or a power-up
         GameObject collectiblePrefab;
         float randomValue = Random.value;
 
         // 50% chance of spawning an obstacle, as opposed to a power-up
-        if (randomValue < 0.5f)
+        if (randomValue <= 0.5f)
         {
             // Spawn an obstacle: equal chance of spawning each
             collectiblePrefab = Random.value < 0.5f ? twigPrefab : rockPrefab;
@@ -47,21 +49,42 @@ public class ObstacleGenerator : MonoBehaviour
         else
         {
             // Spawn a power-up
+
+
             float powerUpRandomValue = Random.value;
-            if (powerUpRandomValue < 0.2f)
+            if (powerUpRandomValue <= 0.4f)
             {
-                // Spawn a red bull: 20% chance out of power-up pool
+                // Spawn a red bull
                 collectiblePrefab = redbullPrefab;
+                fixedSpawnPosition = new Vector3(50f, 14.5f, 22f);
+                Debug.Log("REDBULL SPAWNED");
             }
-            else if (powerUpRandomValue < 0.6f)
-            {
-                // Spawn cheese: 40% chance out of power-up pool
-                collectiblePrefab = cheesePrefab;
-            }
+            // Normal power-up (non-redbull)
             else
             {
-                // Spawn carrot: 40% chance out of power-up pool
-                collectiblePrefab = carrotPrefab;
+
+                
+
+                if (powerUpRandomValue <= 0.6f)
+                {
+                    // Floating (need to jump to get)
+                    fixedSpawnPosition = new Vector3(50f, 14.5f, 23f);
+
+                    // Spawn cheese: 40% chance out of power-up pool
+                    collectiblePrefab = cheesePrefab;
+
+                    
+                }
+                else
+                {
+                    // Resting on bottom of wheel
+                    fixedSpawnPosition = new Vector3(50f, 14.5f, 24.5f);
+
+                    // Spawn carrot: 40% chance out of power-up pool
+                    collectiblePrefab = carrotPrefab;
+
+                    
+                }
             }
         }
 
@@ -89,7 +112,4 @@ public class ObstacleGenerator : MonoBehaviour
         Destroy(collectibleInstance, 12f);
     }
 }
-
-
-
 
---
+++ Assets/Scripts/PowerUpMovement.cs
@@ -0,0 +1,29 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpMovement : MonoBehaviour
+{
+    public float spinSpeed = 100f; // Speed of spinning
+    public float bobHeight = 0.5f; // Height of bobbing
+    public float bobSpeed = 1f; // Speed of bobbing
+
+    private Vector3 localStartPos;
+
+    void Start()
+    {
+        // Store the local starting position relative to the parent
+        localStartPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+
+        // Bob the power-up up and down
+
+        float newZ = localStartPos.z + Mathf.Sin(Time.time * bobSpeed) * bobHeight;
+        transform.localPosition = new Vector3(localStartPos.x, localStartPos.y, newZ);
+    }
+}
+
